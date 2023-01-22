dd4cba697270559799ae0e5a065e94af32b85ba3
Rubanov Andrey <nirwash@bk.ru>
2023-01-22 18:34:14 +0300

    api and db setup

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -0,0 +1,10 @@
+package com.plcoding.stockmarketapp.data.mapper
+
+import com.plcoding.stockmarketapp.data.local.CompanyListingEntity
+import com.plcoding.stockmarketapp.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing() =
+    CompanyListing(name = name, symbol = symbol, exchange = exchange)
+
+fun CompanyListing.toCompanyListingEntity() =
+    CompanyListingEntity(name = name, symbol = symbol, exchange = exchange)
